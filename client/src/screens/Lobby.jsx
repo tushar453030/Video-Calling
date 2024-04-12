121ddc8171afae1cb2955df63b5a7fc82d687cf1
Tushar Sharma <tushar4530@gmail.com>
2024-04-12 23:58:56 +0530

    css nodemailer

--- client/src/screens/Lobby.jsx
+++ client/src/screens/Lobby.jsx
@@ -1,61 +1,90 @@
-import React, { useState, useCallback, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSocket } from "../context/SocketProvider";
+import React, { useState, useCallback, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSocket } from '../context/SocketProvider'
+import '../screens/style.css'
 
 const LobbyScreen = () => {
-  const [email, setEmail] = useState("");
-  const [room, setRoom] = useState("");
+  const [email, setEmail] = useState('')
+  const [room, setRoom] = useState('')
 
-  const socket = useSocket();
-  const navigate = useNavigate();
+  const socket = useSocket()
+  const navigate = useNavigate()
 
   const handleSubmitForm = useCallback(
     (e) => {
-      e.preventDefault();
-      socket.emit("room:join", { email, room });
+      e.preventDefault()
+      socket.emit('room:join', { email, room })
     },
     [email, room, socket]
-  );
+  )
 
   const handleJoinRoom = useCallback(
     (data) => {
-      const { email, room } = data;
-      navigate(`/room/${room}`);
+      const { email, room } = data
+      navigate(`/room/${room}`)
     },
     [navigate]
-  );
+  )
 
   useEffect(() => {
-    socket.on("room:join", handleJoinRoom);
+    socket.on('room:join', handleJoinRoom)
     return () => {
-      socket.off("room:join", handleJoinRoom);
-    };
-  }, [socket, handleJoinRoom]);
+      socket.off('room:join', handleJoinRoom)
+    }
+  }, [socket, handleJoinRoom])
 
   return (
-    <div>
-      <h1>Lobby</h1>
-      <form onSubmit={handleSubmitForm}>
-        <label htmlFor="email">Email ID</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <br />
-        <label htmlFor="room">Room Number</label>
-        <input
-          type="text"
-          id="room"
-          value={room}
-          onChange={(e) => setRoom(e.target.value)}
-        />
-        <br />
-        <button>Join</button>
-      </form>
-    </div>
-  );
-};
+    <div className='lobby-container'>
+      <div className='container'>
+        <nav className='navbar'>
+          <div id='trapezoid'>
+            <a
+              href='https://www.linkedin.com/in/tushar-sharma-a4a032195/'
+              className='expandHome'
+            >
+              LinkedIN
+            </a>
 
-export default LobbyScreen;
+            <a href='https://codepen.io/pec-man' className='expandHome'>
+              Contact
+            </a>
+
+            <a href='https://codepen.io/pec-man' className='expandHome'>
+              Connect
+            </a>
+
+            <a href='https://codepen.io/pec-man' className='expandHome'>
+              Email
+            </a>
+          </div>
+        </nav>
+        <h1 className='title'>Lobby</h1>
+        <form className='form' onSubmit={handleSubmitForm}>
+          <div className='email'>
+            <input
+              placeholder='Email'
+              type='email'
+              id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='room'>
+            <input
+              type='text'
+              id='room'
+              value={room}
+              placeholder='Room Number'
+              onChange={(e) => setRoom(e.target.value)}
+            />
+          </div>
+          <button className='Join'>
+            <span className='spanJoin'>Join</span>
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default LobbyScreen
